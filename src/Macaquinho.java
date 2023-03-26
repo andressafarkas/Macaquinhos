e890ab30d7a3861d7b650b08b8027b26541aa113
Lucas Alves <lucas.soares008@edu.pucrs.br>
2023-03-26 12:42:04 -0300

    Estrutura da classe Macaquinho

--- src/Macaquinho.java
+++ src/Macaquinho.java
@@ -1,7 +1,62 @@
-public class Macaquinho{
+import java.util.ArrayList;
+import java.util.List;
 
-    private Coco coco;
-    private Macaquinho direita;
-    private Macaquinho esquerda;
+public class Macaquinho
+{
+
+    private List<Coco> cocos;
+    private final int macaquinhoId;
+    private Macaquinho macaquinhoPar;
+    private Macaquinho macaquinhoImpar;
+
+    public Macaquinho(List<Coco> cocos, int macaquinhoId, Macaquinho macacoPar, Macaquinho macacoImpar)
+    {
+        this.cocos = new ArrayList<>(20);
+        this.macaquinhoId = macaquinhoId;
+        this.macaquinhoPar = macacoPar;
+        this.macaquinhoImpar = macacoImpar;
+    }
+
+    public void receberCoco(Coco coco)
+    {
+        this.cocos.add(coco);
+    }
+
+    public void jogar()
+    {
+        for (Coco coco : cocos)
+        {
+            if (coco.getPedrinhas() % 2 == 0)
+            {
+                macaquinhoPar.receberCoco(coco);
+            } else
+            {
+                macaquinhoImpar.receberCoco(coco);
+            }
+        }
+        cocos.clear();
+    }
+
+    public int getTotalCocos()
+    {
+        return cocos.size();
+    }
+
+    public int getMacaquinhoIdId()
+    {
+        return macaquinhoId;
+    }
+
+    public void setMacaquinhoPar(Macaquinho macaquinhoPar)
+    {
+        this.macaquinhoPar = macaquinhoPar;
+    }
+
+    public void setMacaquinhoImpar(Macaquinho macaquinhoImpar)
+    {
+        this.macaquinhoImpar = macaquinhoImpar;
+    }
+
+
 
 }
