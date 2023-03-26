a6ee1371d68594b4b2052fcda55d112522b932a3
Lucas Alves <lucas.soares008@edu.pucrs.br>
2023-03-26 12:22:26 -0300

    Project Structure

---
+++ src/Coco.java
@@ -0,0 +1,13 @@
+public class Coco {
+
+    private final int pedrinhas;
+
+    public Coco(int pedrinhas) {
+        this.pedrinhas = pedrinhas;
+    }
+    
+    public boolean Par(int pedrinhas)
+    {
+        return pedrinhas % 2 == 0;
+    }
+}
