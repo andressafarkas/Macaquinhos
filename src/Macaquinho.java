456b12c0731158d9d047c85f75f7285d06155da5
Lucas Alves <lucas.soares008@edu.pucrs.br>
2023-03-26 13:27:49 -0300

    Método que soma todas as pedrinhas do macaquinho

--- src/Macaquinho.java
+++ src/Macaquinho.java
@@ -42,6 +42,16 @@ public class Macaquinho
         return cocos.size();
     }
 
+    public int getNumPedrinhas()
+    {
+        int pedrinhas = 0;
+        for (Coco coco: cocos)
+        {
+            pedrinhas += coco.getPedrinhas();
+        }
+        return pedrinhas;
+    }
+
     public int getMacaquinhoId()
     {
         return macaquinhoId;
