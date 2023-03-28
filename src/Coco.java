d09df8bea7a1e64981ea2cb5fbd3fbc38dc2ad91
22106564 <lucas.soares008@edu.pucrs.br>
2023-03-28 20:19:29 -0300

    removi método inutilizado

--- src/Coco.java
+++ src/Coco.java
@@ -5,11 +5,6 @@ public class Coco {
     public Coco(int pedrinhas) {
         this.numPedrinhas = pedrinhas;
     }
-    
-    public boolean Par(int pedrinhas)
-    {
-        return pedrinhas % 2 == 0;
-    }
 
     public int getPedrinhas()
     {
